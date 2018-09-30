0ef25779bb1e95f71bc8040e74197d67c9b4f424
mloves0824 <mloves0824@163.com>
2018-09-30 13:46:57 +0800

    test

--- cmd/fe/main.go
+++ cmd/fe/main.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-//	"github.com/docopt/docopt-go"
+	"github.com/martini-contrib/render"
+
+	//	"github.com/docopt/docopt-go"
 )
 
 func main() {
-//	const usage = `
-//Usage:
-//	antalk-web-fe [--ncpu=N] [--log=FILE] [--log-level=LEVEL] [--assets-dir=PATH] [--pidfile=FILE]  --listen=ADDR
-//	antalk-web-fe  --version
-//
-//Options:
-//	--ncpu=N                        set runtime.GOMAXPROCS to N, default is runtime.NumCPU().
-//	-l FILE, --log=FILE             set path/name of daliy rotated log file.
-//	--log-level=LEVEL               set the log-level, should be INFO,WARN,DEBUG or ERROR, default is INFO.
-//	--listen=ADDR                   set the listen address.
-//`
-//	d, err := docopt.Parse(usage, nil, true, "", false)
-//	if err != nil {
-//		//log.PanicError(err, "parse arguments failed")
-//		return 
-//	}
-	m := martini.Classic()
-	m.Get("/", func() string {
-    		return "Hello world!"
-  	})
-  	m.Run()
+	//	const usage = `
+	//Usage:
+	//	antalk-web-fe [--ncpu=N] [--log=FILE] [--log-level=LEVEL] [--assets-dir=PATH] [--pidfile=FILE]  --listen=ADDR
+	//	antalk-web-fe  --version
+	//
+	//Options:
+	//	--ncpu=N                        set runtime.GOMAXPROCS to N, default is runtime.NumCPU().
+	//	-l FILE, --log=FILE             set path/name of daliy rotated log file.
+	//	--log-level=LEVEL               set the log-level, should be INFO,WARN,DEBUG or ERROR, default is INFO.
+	//	--listen=ADDR                   set the listen address.
+	//`
+	//	d, err := docopt.Parse(usage, nil, true, "", false)
+	//	if err != nil {
+	//		//log.PanicError(err, "parse arguments failed")
+	//		return
+	//	}
+	//	m := martini.Classic()
+	//	m.Get("/", func() string {
+	//		return "Hello world!"
+	//	})
+	//	m.Run()
+
+	m := martini.New()
+	m.Use(martini.Recovery())
+	m.Use(render.Renderer())
+	m.Use(martini.Static(assets, martini.StaticOptions{SkipLogging: true}))
+	m.Run()
 }
