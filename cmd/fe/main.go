549d6e5016765a4a64ddb3c6a8d64ae443a767b8
Ubuntu-dev <mloves0824@163.com>
2018-10-06 14:50:25 +0800

    tst

--- cmd/fe/main.go
+++ cmd/fe/main.go
@@ -2,38 +2,65 @@ package main
 
 import (
 	"path/filepath"
-//	"net/http"
-
+	//	"net/http"
+	"log"
+	"time"
 
 	"github.com/go-martini/martini"
-//	"github.com/martini-contrib/render"
-//	"github.com/mloves0824/antalk_web/pkg/test"
+	//	"github.com/martini-contrib/render"
+	//	"github.com/mloves0824/antalk_web/pkg/test"
 	//	"github.com/docopt/docopt-go"
+	pb "github.com/mloves0824/antalk_web/pkg/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 import "net/http"
 import "github.com/gin-gonic/gin"
 
-func main() {
-        router := gin.Default()
-        router.LoadHTMLGlob("/home/messi/dev/go/src/github.com/mloves0824/antalk_web/cmd/fe/templates/*")
-        //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-        router.GET("/index", func(c *gin.Context) {
-                c.HTML(http.StatusOK, "index.tmpl", gin.H{
-                        "title": "Main website",
-                })
-        })
-        // Query string parameters are parsed using the existing underlying request object.
-        // The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
-        router.GET("/test/pegasus/set", func(c *gin.Context) {
-                firstname := c.DefaultQuery("key", "Guest")
-                lastname := c.Query("value") // shortcut for c.Request.URL.Query().Get("lastname")
-        
-                c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
-        })
-        router.Run(":3000")
-        
-}  
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
 
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("/home/chenb/workspace/github/mnt/gopath/src/github.com/mloves0824/antalk_web/cmd/fe/templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+	// Query string parameters are parsed using the existing underlying request object.
+	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+	router.GET("/test/pegasus/set", func(c *gin.Context) {
+		key := c.DefaultQuery("key", "Guest")
+		value := c.Query("value") // shortcut for c.Request.URL.Query().Get("lastname")
+
+		// Set up a connection to the server.
+		conn, err := grpc.Dial(address, grpc.WithInsecure())
+		if err != nil {
+			log.Printf("did not connect: %v", err)
+			c.String(http.StatusInternalServerError, "error: set %s %s", key, value)
+			return
+		}
+		defer conn.Close()
+		client := pb.NewTestPegasusClient(conn)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err := client.TestSet(ctx, &pb.SetReq{Key: key, Value: value})
+		if err != nil {
+			log.Printf("could not set: %v", err)
+			c.String(http.StatusInternalServerError, "error %s %s", key, value)
+			return
+		}
+		log.Printf("Status: %s", r.Status)
+		c.String(http.StatusOK, "Hello %s %s", key, value)
+	})
+	router.Run(":3000")
+
+}
 
 func main_martini() {
 	//	const usage = `
@@ -66,51 +93,50 @@ func main_martini() {
 	}
 	assets = abspath
 
-	println(assets);
+	println(assets)
 
-//	api := &test.ApiServer{}
-//
-//	m := martini.New()
-//	m.Use(martini.Recovery())
-//	m.Use(render.Renderer())
-//	m.Use(martini.Static(assets, martini.StaticOptions{SkipLogging: true}))
-//	m.Use(func(c martini.Context, w http.ResponseWriter) {
-//		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-//	})
-//	//r := martini.NewRouter()
-//	//m.Group("/test/pegasus", func(r martini.Router) {
-////		r.Get("/set/:key", api.Set)
-//	//})
-////	m.Get("/list", api.List)
-//
-//	//m.MapTo(r, (*martini.Routes)(nil))
-//	//m.Action(r.Handle)
-//
-//	//m.Get("/hello/:name", func(params martini.Params) string {
-//  	//	return "Hello " + params["name"]
-//	//})
-//
-//	m.Get("/hello", func() string {
-//  		return "hello world" // HTTP 200 : "hello world"
-//	})
-//
-//	m.Run()
-	
+	//	api := &test.ApiServer{}
+	//
+	//	m := martini.New()
+	//	m.Use(martini.Recovery())
+	//	m.Use(render.Renderer())
+	//	m.Use(martini.Static(assets, martini.StaticOptions{SkipLogging: true}))
+	//	m.Use(func(c martini.Context, w http.ResponseWriter) {
+	//		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	//	})
+	//	//r := martini.NewRouter()
+	//	//m.Group("/test/pegasus", func(r martini.Router) {
+	////		r.Get("/set/:key", api.Set)
+	//	//})
+	////	m.Get("/list", api.List)
+	//
+	//	//m.MapTo(r, (*martini.Routes)(nil))
+	//	//m.Action(r.Handle)
+	//
+	//	//m.Get("/hello/:name", func(params martini.Params) string {
+	//  	//	return "Hello " + params["name"]
+	//	//})
+	//
+	//	m.Get("/hello", func() string {
+	//  		return "hello world" // HTTP 200 : "hello world"
+	//	})
+	//
+	//	m.Run()
 
 	m := martini.Classic()
-      //m.Use(martini.Recovery())
-      //m.Use(render.Renderer())
-      //m.Use(martini.Static(assets, martini.StaticOptions{SkipLogging: true}))
-      //m.Use(func(c martini.Context, w http.ResponseWriter) {
-      //        w.Header().Set("Content-Type", "application/json; charset=utf-8")
-      //})
+	//m.Use(martini.Recovery())
+	//m.Use(render.Renderer())
+	//m.Use(martini.Static(assets, martini.StaticOptions{SkipLogging: true}))
+	//m.Use(func(c martini.Context, w http.ResponseWriter) {
+	//        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	//})
 
-  	// ... middleware and routing goes here
+	// ... middleware and routing goes here
 	//m.Get("/hello", func() string {
 	//	return "hello world"
-	//})	
+	//})
 	m.Get("/hello1/:name", func(params martini.Params) string {
- 		return "Hello " + "name"
+		return "Hello " + "name"
 	})
-	 m.Run()	
+	m.Run()
 }
--- cmd/test/pegasus/main.go
+++ cmd/test/pegasus/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"path/filepath"
+	"time"
 
+	"github.com/XiaoMi/pegasus-go-client/pegasus"
+	pb "github.com/mloves0824/antalk_web/pkg/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	pb "github.com/mloves0824/antalk_web/pkg/proto"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -17,6 +24,32 @@ const (
 type server struct{}
 
 func (s *server) TestSet(ctx context.Context, in *pb.SetReq) (*pb.SetReply, error) {
+
+	cfgPath, _ := filepath.Abs("./config/pegasus-client-config.json")
+	rawCfg, err := ioutil.ReadFile(cfgPath)
+	if err != nil {
+		//fmt.Println(err)
+		return &pb.SetReply{Status: -1}, nil
+	}
+
+	cfg := &pegasus.Config{}
+	json.Unmarshal(rawCfg, cfg)
+	c := pegasus.NewClient(*cfg)
+
+	tb, err := c.OpenTable(context.Background(), "temp")
+	if err != nil {
+		return &pb.SetReply{Status: -2}, nil
+	}
+
+	time.Sleep(time.Second * 300)
+
+	value := make([]byte, 1000)
+	for i := 0; i < 1000; i++ {
+		value[i] = 'x'
+	}
+	//in.Key
+	tb.Set(context.Background(), []byte("hash"), []byte("sort"), value)
+
 	return &pb.SetReply{Status: 0}, nil
 }
 
